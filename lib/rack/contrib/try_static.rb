9ec7d0a8867c1ce2ecdbba22e10e3a8bb3dc9403
gmarik <gmarik@gmail.com>
2010-09-19 11:02:58 -0500

    Fixed bug

--- lib/rack/contrib/try_static.rb
+++ lib/rack/contrib/try_static.rb
@@ -1,24 +1,26 @@
 require 'rack/static'
 
 module ::Rack
-  class TryStatic < Static
+  class TryStatic
 
     def initialize(app, options)
-      super
+      @app = app
+      @static = ::Rack::Static.new(lambda {[404, {}, []]}, options)
       @try = ([''] + Array(options.delete(:try)) + [''])
     end
 
     def call(env)
       @next = 0
-      while @next < @try.size && 404 == (resp = super(try_next(env)))[0] 
+      while @next < @try.size && 404 == (resp = @static.call(try_next(env)))[0] 
         @next += 1
       end
-      404 == resp[0] ? @app.call : resp
+      404 == resp[0] ? @app.call(env) : resp
     end
 
     private
     def try_next(env)
-      env.merge('PATH_INFO' => env['PATH_INFO'] + @try[@next])
+      try_path = env['PATH_INFO'] + @try[@next]
+      env.merge('PATH_INFO' => try_path)
     end
   end
 end
