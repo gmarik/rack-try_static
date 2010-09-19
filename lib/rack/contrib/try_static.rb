e9459f15236bfb9614b45fa68f9564aa9f00ee86
gmarik <gmarik@gmail.com>
2010-09-19 11:48:51 -0500

    Improvements

--- lib/rack/contrib/try_static.rb
+++ lib/rack/contrib/try_static.rb
@@ -5,22 +5,20 @@ module ::Rack
 
     def initialize(app, options)
       @app = app
-      @static = ::Rack::Static.new(lambda {[404, {}, []]}, options)
-      @try = ([''] + Array(options.delete(:try)) + [''])
+      @try = [*options.delete(:try)]
+      @static = ::Rack::Static.new(
+        lambda { [404, {}, []] }, 
+        options)
     end
 
     def call(env)
-      @next = 0
-      while @next < @try.size && 404 == (resp = @static.call(try_next(env)))[0] 
-        @next += 1
-      end
-      404 == resp[0] ? @app.call(env) : resp
-    end
-
-    private
-    def try_next(env)
-      try_path = env['PATH_INFO'] + @try[@next]
-      env.merge('PATH_INFO' => try_path)
+      orig_path = env['PATH_INFO']
+      found = nil
+      @try.each do |path|
+        resp = @static.call(env.merge!({'PATH_INFO' => orig_path + path}))
+        break if 404 != resp[0] && found = resp
+      end 
+      found or @app.call(env.merge!('PATH_INFO' => orig_path))
     end
   end
 end
